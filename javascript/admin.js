709a440e03a264d18219ff8957a41ee38bcb65e9
Checkr <checkr2025@gmail.com>
2025-04-12 13:59:53 +0300

    Add files via upload

--- javascript/admin.js
+++ javascript/admin.js
@@ -5,7 +5,7 @@ function login() {
     // Credentials
     if (username === "Administrator Ndejje Senior Secondary School" && password === "@dminPazzw0rd123") {
         sessionStorage.setItem("authenticated", "true");
-        window.location.href = "submit.html";
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSdCZbM1fX4Xqn74qdFfeYlpGTg7AhJT3GEoBPo6Gz_LTZahfQ/viewform?usp=sharing";
     } else {
         alert("Invalid credentials. Try again.");
     }
